35c41c4ab32ede452f207e27c616296ff1c0714f
emancipatedMind <powensfinch@gmail.com>
2016-08-19 19:22:09 -0400

    Moved PortExtensions into namespace.

--- XModemCommunicator - Sender Methods.cs
+++ XModemCommunicator - Sender Methods.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CSharpToolkit;
 
 namespace XModemProtocol {
     public partial class XModemCommunicator {
--- XmodemCommunicator - Utility Methods.cs
+++ XmodemCommunicator - Utility Methods.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CSharpToolkit;
 
 namespace XModemProtocol {
     public partial class XModemCommunicator {
