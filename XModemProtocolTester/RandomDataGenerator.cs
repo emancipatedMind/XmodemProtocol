b69ae046d45904ec584deccf87abeb190a96d20b
emancipatedMind <powensfinch@gmail.com>
2016-11-03 17:02:06 -0400

    Cleaned up RandomDataGenerator, and gave Domain, and Offset defaults. Produces uppercase and lowercase letters of the alphabet by default.

--- XModemProtocolTester/RandomDataGenerator.cs
+++ XModemProtocolTester/RandomDataGenerator.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 
 namespace XModemProtocolTester {
     public class RandomDataGenerator {
-        Random rand = new Random();
-        List<byte> data;
-        public int Offset { get; set; }
-        public int Domain { get; set; }
+        static Random rand = new Random();
+        public int Offset { get; set; } = 0x20;
+        public int Domain { get; set; } = 0x5E;
 
         public IEnumerable<byte> GetRandomData(int length) {
-            data = new List<byte>();
+            var data = new List<byte>();
             for (int i = 0; i < length; i++) 
                 data.Add((byte)(rand.Next(Domain) + Offset));
             return data;
