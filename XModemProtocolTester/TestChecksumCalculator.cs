f6ea753fd8d96824cc8ba147b7bd68a058ce9d53
emancipatedMind <powensfinch@gmail.com>
2016-11-03 17:26:43 -0400

    Cleaned up TestChecksumCalculator.

--- XModemProtocolTester/TestChecksumCalculator.cs
+++ XModemProtocolTester/TestChecksumCalculator.cs
@@ -2,8 +2,7 @@
 using System.Linq;
 using XModemProtocol.Calculators;
 
-namespace XModemProtocolTester
-{
+namespace XModemProtocolTester {
     [TestFixture] 
     public class TestChecksumCalculator {
 
@@ -13,13 +12,14 @@ namespace XModemProtocolTester
             byte[] collection = new byte[] { 0x00, 0x19, 0x88, 0x33, 0x72, };
             byte actualChecksum = (byte)collection.Sum(currentByte => currentByte);
             byte calculatedChecksum = _calculator.CalculateChecksum(collection).ElementAt(0);
+            // Test to see if calculator properly calculates checksum.
             Assert.AreEqual(actualChecksum, calculatedChecksum);
-            Assert.AreNotEqual(0x15, calculatedChecksum);
         }
 
         [Test]
         public void TestCRCChecksumCalculator() {
             ICRCChecksumCalculator _calculator = new CRCChecksumCalculator(new LookUpTable(0x1021));
+            // Test to see if calculator properly calculates checksum.
             Assert.True(Enumerable.SequenceEqual(_calculator.CalculateChecksum(new byte[] { 0xD9 }), new byte[] { 0x5A, 0x54 }));
             Assert.True(Enumerable.SequenceEqual(_calculator.CalculateChecksum(new byte[] { 0xA5 }), new byte[] { 0xE5, 0x4F }));
             Assert.True(Enumerable.SequenceEqual(_calculator.CalculateChecksum(new byte[] { 0xE5, 0xAD, 0x8B }), new byte[] { 0x00, 0x00 }));
