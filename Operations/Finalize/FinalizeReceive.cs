024ea80735978394d7ee40ff5cfcde2c34479986
emancipatedMind <powensfinch@gmail.com>
2016-11-05 19:39:54 -0400

    FinalizeReceive will no longer run, and throw error if no data exists.

--- Operations/Finalize/FinalizeReceive.cs
+++ Operations/Finalize/FinalizeReceive.cs
@@ -1,6 +1,7 @@
 ﻿namespace XModemProtocol.Operations.Finalize {
     public class FinalizeReceive : Finalizer {
         protected override void FinalizeOp() {
+            if (_context.Data.Count < 1) return; 
             for (int i = _context.Data.Count - 1; _context.Data[i] == _context.Options.SUB; i--)
                 _context.Data.RemoveAt(i); 
         }
