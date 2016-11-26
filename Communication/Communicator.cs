0d84715d3445a156ff5e10c2aa42570396b814cf
emancipatedMind <powensfinch@gmail.com>
2016-11-26 13:47:39 -0500

    Communicator now has a property to set the SerialPort, and default constructor has been reimplemented.

--- Communication/Communicator.cs
+++ Communication/Communicator.cs
@@ -4,43 +4,44 @@
     using System.IO.Ports;
     public class Communicator : ICommunicator {
 
-        SerialPort _port;
-
         public Communicator(SerialPort port) {
-            _port = port;
+            Port = port;
         }
+        public Communicator() : this(new SerialPort()) { }
 
-        public virtual int BytesInReadBuffer => _port.BytesToRead;
+        public SerialPort Port { set; private get; }
+
+        public virtual int BytesInReadBuffer => Port.BytesToRead;
 
         public virtual void Flush() {
-            _port.DiscardInBuffer();
-            _port.DiscardOutBuffer();
+            Port.DiscardInBuffer();
+            Port.DiscardOutBuffer();
         }
 
         public virtual List<byte> ReadAllBytes() {
             List<byte> byteList = new List<byte>();
             byte[] byteArray;
             do {
-                int bytesToRead = _port.BytesToRead;
+                int bytesToRead = Port.BytesToRead;
                 byteArray = new byte[bytesToRead];
-                _port.Read(byteArray, 0, bytesToRead);
+                Port.Read(byteArray, 0, bytesToRead);
                 byteList.AddRange(byteArray);
                 System.Threading.Tasks.Task.Delay(10); 
-            } while (_port.BytesToRead > 0);
+            } while (Port.BytesToRead > 0);
             return byteList;
         }
 
         public virtual void Write(IEnumerable<byte> buffer) {
             byte[] bufferArray = buffer.ToArray();
-            _port.Write(bufferArray, 0, bufferArray.Length);
+            Port.Write(bufferArray, 0, bufferArray.Length);
         }
 
         public virtual void Write(byte buffer) {
-            _port.Write(new byte[] { buffer }, 0, 1);
+            Port.Write(new byte[] { buffer }, 0, 1);
         }
 
         public virtual byte ReadSingleByte() {
-            return (byte) _port.ReadByte();
+            return (byte) Port.ReadByte();
         }
     }
 }
