c5a7b6b5ac3bc25cfeb8916c6ba2c9918d71be28
emancipatedMind <powensfinch@gmail.com>
2017-07-01 14:34:24 -0400

    Changed test for LookupTable to easily allow another class to be tested by the same metrics.

--- XModemProtocolTester/TestCRCLookupTable.cs
+++ XModemProtocolTester/TestCRCLookupTable.cs
@@ -7,17 +7,23 @@ namespace XModemProtocolTester {
     public class TestCRCLookupTable {
         [Test] 
         public void LookupTableTest() {
+
+            int polynomial = 0x1021;
+
             List<int[]> indicesToCheck = new List<int[]> {
                 new int[] { 0, 1, 187, 254, 255, },
                 new int[] { 0, 4129, 5808, 3793, 7920, },
             };
 
-            ICRCLookUpTable table = new LookUpTable(0x1021);
+            ICRCLookUpTable[] tables = new ICRCLookUpTable[] {
+                new LookUpTable(polynomial),
+            };
 
-            for(int i = 0; i < indicesToCheck[0].Length; i++) {
-                // Test Lookup table by testing random indices to be sure value is correct.
-                Assert.AreEqual(table.QueryTable(indicesToCheck[0][i]), indicesToCheck[1][i]); 
-            }
+            foreach (var t in tables) 
+                for(int i = 0; i < indicesToCheck[0].Length; i++) {
+                    // Test Lookup table by testing random indices to be sure value is correct.
+                    Assert.AreEqual(t.QueryTable(indicesToCheck[0][i]), indicesToCheck[1][i]); 
+                }
         }
     }
 }
