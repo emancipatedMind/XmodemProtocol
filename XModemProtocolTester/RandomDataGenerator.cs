363aaff1733140460258aa0af9153c5d15a9fc11
emancipatedMind <powensfinch@gmail.com>
2017-07-08 19:16:47 -0400

    Refactored RandomDataGenerator.

--- XModemProtocolTester/RandomDataGenerator.cs
+++ XModemProtocolTester/RandomDataGenerator.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace XModemProtocolTester {
+﻿namespace XModemProtocolTester {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     public class RandomDataGenerator {
+
         static Random rand = new Random();
         public int Offset { get; set; } = 0x20;
         public int Domain { get; set; } = 0x5E;
 
-        public IEnumerable<byte> GetRandomData(int length) {
-            var data = new List<byte>();
-            for (int i = 0; i < length; i++) 
-                data.Add((byte)(rand.Next(Domain) + Offset));
-            return data;
-        }
+        public IEnumerable<byte> GetRandomData(int length) =>
+            Enumerable.Range(0, length).Select(x => (byte)(rand.Next(Domain) + Offset)).ToArray();
     }
 }
