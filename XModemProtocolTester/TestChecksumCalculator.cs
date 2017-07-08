771e06f39ea5fff27a90cbe4a82d36f030f83f40
emancipatedMind <powensfinch@gmail.com>
2017-07-08 18:16:24 -0400

    Refactored the test once again for the ChecksumCalculator.

--- XModemProtocolTester/TestChecksumCalculator.cs
+++ XModemProtocolTester/TestChecksumCalculator.cs
@@ -3,47 +3,63 @@
     using System.Linq;
     using System.Collections.Generic;
     using XModemProtocol.Calculators;
-    [TestFixture] 
+    [TestFixture]
     public class TestChecksumCalculator {
 
-        [Test] 
+        [Test]
         public void NormalChecksumCalculatorTest() {
-            ISummationChecksumCalculator _calculator = new NormalChecksumCalculator();
-            byte[] collection = new byte[] { 0x00, 0x19, 0x88, 0x33, 0x72, };
-            byte actualChecksum = (byte)collection.Sum(currentByte => currentByte);
-            byte calculatedChecksum = _calculator.CalculateChecksum(collection).ElementAt(0);
-            // Test to see if calculator properly calculates checksum.
-            Assert.AreEqual(actualChecksum, calculatedChecksum);
+            PerformCheckSumCalculatorTest(
+                new byte[][] {
+                    new byte[] { 0x00, 0x19, 0x88, 0x33, 0x72, },
+                },
+                new byte[][] {
+                        new byte[] { 0x46, },
+                },
+                new List<ISummationChecksumCalculator> {
+                    new NormalChecksumCalculator(),
+            });
         }
 
         [Test]
         public void CRCChecksumCalculatorTest() {
-            new byte[][] {
-                new byte[] { 0xD9 },
-                new byte[] { 0xA5 },
-                new byte[] { 0xE5, 0xAD, 0x8B },
-            }
-            .Zip(new byte[][] {
-                    new byte[] { 0x5A, 0x54 },
-                    new byte[] { 0xE5, 0x4F },
-                    new byte[] { 0x00, 0x00 },
+            PerformCheckSumCalculatorTest(
+                new byte[][] {
+                    new byte[] { 0xD9 },
+                    new byte[] { 0xA5 },
+                    new byte[] { 0xE5, 0xAD, 0x8B },
                 },
+                new byte[][] {
+                        new byte[] { 0x5A, 0x54 },
+                        new byte[] { 0xE5, 0x4F },
+                        new byte[] { 0x00, 0x00 },
+                },
+                new List<ICRCChecksumCalculator> {
+                    new CRCChecksumCalculator(new FunctionalLookUpTable(0x1021)),
+                    new FunctionalCRCChecksumCalculator(new FunctionalLookUpTable(0x1021)),
+            });
+        }
+
+        private void PerformCheckSumCalculatorTest(
+            IEnumerable<IEnumerable<byte>> collection,
+            IEnumerable<IEnumerable<byte>> checksum,
+            IEnumerable<IChecksumCalculator> calculators
+        ) {
+            collection
+            .Zip(
+                checksum,
                 CombineCollectionWithCheckSum
             )
             .ToList()
             .ForEach(test =>
-                new List<ICRCChecksumCalculator> {
-                    new CRCChecksumCalculator(new FunctionalLookUpTable(0x1021)),
-                    new FunctionalCRCChecksumCalculator(new FunctionalLookUpTable(0x1021)),
-                }
+                calculators
+                .ToList()
                 .ForEach(c =>
                     Assert.AreEqual(test.CheckSum, c.CalculateChecksum(test.Collection))
                 ));
         }
 
-        private (byte[] Collection, byte[] CheckSum) CombineCollectionWithCheckSum(byte[] coll, byte[] checksum) =>
+        private (IEnumerable<byte> Collection, IEnumerable<byte> CheckSum) CombineCollectionWithCheckSum(IEnumerable<byte> coll, IEnumerable<byte> checksum) =>
             (coll, checksum);
 
     }
-
 }
