ecdc75c70a4fa4c9616dce85387f2dfb56e4a856
emancipatedMind <powensfinch@gmail.com>
2017-07-08 18:20:53 -0400

    Added new class FunctionalNormalChecksumCalculator.

---
+++ Calculators/FunctionalNormalChecksumCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace XModemProtocol.Calculators {
+    using System.Collections.Generic;
+    using System.Linq;
+    public class FunctionalNormalChecksumCalculator : ISummationChecksumCalculator {
+
+        public IEnumerable<byte> CalculateChecksum(IEnumerable<byte> input) =>
+            new byte[] { (byte)input.Sum(b => b) };
+
+    }
+}
